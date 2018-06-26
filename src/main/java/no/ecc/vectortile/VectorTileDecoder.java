c8f27b84f2ae668142cac6c697685c18f99a1068
Jim Hughes <jnh5y@ccri.com>
2018-06-26 17:40:29 -0400

    Upgraded to JTS 1.15.0.
    Signed-off-by: Jim Hughes <jnh5y@ccri.com>

--- src/main/java/no/ecc/vectortile/VectorTileDecoder.java
+++ src/main/java/no/ecc/vectortile/VectorTileDecoder.java
@@ -34,11 +34,11 @@ import java.util.Set;
 import vector_tile.VectorTile;
 import vector_tile.VectorTile.Tile.Layer;
 
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.geom.GeometryFactory;
-import com.vividsolutions.jts.geom.LineString;
-import com.vividsolutions.jts.geom.LinearRing;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.LineString;
+import org.locationtech.jts.geom.LinearRing;
 
 public class VectorTileDecoder {
 
